338aa7815e28a79134783456c4efe95c0439971d
alencartha <alencar_tha@hotmail.com>
2021-03-27 19:16:30 -0300

    products adicionados

--- server/controller/productsController.js
+++ server/controller/productsController.js
@@ -1,55 +1,99 @@
+const dataBase = require("../db/models");
+
 //RETORNA TODOS OS PRODUTOS
 const getProducts = (req, res) => {
-    res.status(200).send({
-      message: "Retorna os produtos",
+  dataBase.Products.findAll()
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch(() =>
+      res.json({
+        message: "Não foi possível processar a operação",
+      })
+    );
+};
+
+//LOCALIZA PRODUTO POR ID
+const getProductById = (req, res) => {
+  dataBase.Products.findAll({ where: { id: req.params.uid } })
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch(() =>
+      res.json({
+        message: "Não foi possível processar a operação",
+      })
+    );
+};
+
+//INSERE UM PRODUTO
+const postProduct = (req, res) => {
+  const { name, price, flavor, complement, image, type, sub_type } = req.body;
+  dataBase.Products.create({
+    name,
+    price,
+    flavor,
+    complement,
+    image,
+    type,
+    sub_type,
+  })
+    .then((result) => {
+      res.status(201).json(result);
+    })
+    .catch(() =>
+      res.json({
+        message: "Não foi possível processar a operação",
+      })
+    );
+};
+
+//ALTERA UM PRODUTO
+const updateProduct = (req, res) => {
+  const { name, price, flavor, complement, image, type, sub_type } = req.body;
+  dataBase.Products.update(
+    {
+      name,
+      price,
+      flavor,
+      complement,
+      image,
+      type,
+      sub_type,
+    },
+    { where: { id: req.params.id } }
+  )
+    .then(() => {
+      res.status(200).json({
+        message: "Dados de usuário atualizados com sucesso!",
+      });
+    })
+    .catch(() => {
+      res.json({
+        message: "Não foi possível processar a operação",
+      });
     });
-  };
-  
-  //LOCALIZA PRODUTO POR ID
-  const getProductById = (req, res) => {
-    const id = req.params.id_order;
-    res.status(200).send({
-      message: "Detalhe do produto, por id",
-      id: id,
+};
+
+//DELETA UM PRODUTO
+const deleteProduct = (req, res) => {
+  dataBase.Users.destroy({ where: { id: req.params.id } })
+    .then(() => {
+      res.status(200).json({
+        message: "Usuário deletado",
+      });
+    })
+    .catch(() => {
+      res.json({
+        message: "Não foi possível processar a operação",
+      });
     });
-  };
-  
-  //INSERE UM PRODUTO
-  const postProduct = (req, res) => {
-    const product = {
-      id_produto: req.body.id_produto,
-      quantidade: req.body.quantidade,
-    };
-    res.status(201).send({
-      message: "Produto criado",
-      produtoCriado: product,
-    });
-  };
-  
-  //ALTERA UM PRODUTO
-  const updateProduct = (req, res) => {
-    const product = {
-      id_produto: req.body.id_produto,
-      quantidade: req.body.quantidade,
-    };
-    res.status(201).send({
-      message: "Produto alterado",
-      produtoAlterado: product,
-    });
-  };
-  
-  //DELETA UM PRODUTO
-  const deleteProduct = (req, res) => {
-    res.status(201).send({
-      message: "Excluir o pedido",
-    });
-  };
-  
-  module.exports = {
-    getProducts,
-    getProductById,
-    postProduct,
-    updateProduct,
-    deleteProduct,
-  };
-  
+};
+
+module.exports = {
+  getProducts,
+  getProductById,
+  postProduct,
+  updateProduct,
+  deleteProduct,
+};
